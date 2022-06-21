3c78d317acdf6ca53ad7f934c36c782cd3f04b9a
peterhenrique <pedrohsdeoliveira27@gmail.com>
2022-06-21 16:46:54 -0300

    lib/game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,211 +1,150 @@
 require 'csv'
 require 'pry-byebug'
+require 'yaml'
 
 class Player
-    attr_accessor :name, :player_choices, :health, :input
-    def initialize (name, health = 5, input = '')
-        @alive = true
-        @name = name
-        @health = health
-        @input = input.split('')
-    end
+  attr_accessor :name, :player_choices, :health, :input
 
-    def alive
-        @alive = true
-    end
-
-
-    def name
-        @name
-    end
-
-    def get_name
-        puts @name
-    end
-
-    def input
-        @input
-    end
-
-    def player_input
-        input = ""
-        puts "Select a letter"        
-        input = gets.chomp
-        until input.length == 1
-            puts "invalid choice, try again"
-            input = gets.chomp
-        end
-        change_input(input)
-        print_input       
-    end
-
-    def change_input(arg)
-        input = @input << arg
-        self.input = input
-    end
-
-    def print_input
-        p @input
-    end
-
-    def choices 
-        @variable
-    end 
-
-    def health
-        @health       
-    end
-
-    def puts_health
-        puts "you have #{self.health} tries remaining"
-    end
-
-    def health_damage
-        self.health = @health - 1
-        self.puts_health
-    end
-    
-    def check_health
-        if self.health == 0
-            self.alive = false
-            game_over
-        end
-    end
-
-         
-
-
-
+  def initialize(name, health = 5, input = '')
+    @alive = true
+    @name = name
+    @health = health
+    @input = input.split('')
+  end
 end
 
 class Dictionary
-    attr_accessor :word
-    @word = ''
-    def initialize
-        @contents = File.readlines('google-10000-english-no-swears.txt')
-        selectoring(@contents)
-    end
+  attr_accessor :word
 
-    def selectoring (array)
-        word = ''        
-        until word.length >= 5 == true && word.length < 13 == true
-            i = rand(0..array.length)
-            word = array[i][0..-2]
-        end
-        @word = word
-        
-    end
+  @word = ''
+  def initialize
+    @contents = File.readlines('google-10000-english-no-swears.txt')
+    selectoring(@contents)
+  end
 
-    def word
-        @word
+  def selectoring(array)
+    word = ''
+    until word.length >= 5 == true && word.length < 13 == true
+      i = rand(0..array.length)
+      word = array[i][0..-2]
     end
- 
+    @word = word
+  end
+
 end
 
-
-
 class Game
-    attr_accessor :name, :alive, :winner, :game_over
-    @@chosen_word = ''
-    @@underscored_word = ''
-    @@player_word = []
+  attr_accessor :name, :alive, :winner, :game_over, :health
 
-    def initialize 
-        puts "Hangman started!"
-        puts 'What is your name?'
-        @name = gets.chomp
-        @alive = true
-        @winner = false
-        @game_over = false
-        new = Player.new(@name.to_s)
-        update_word
-        new.puts_health
-        update_underscored
-        while @alive == true
-            jk = new.health
-            check_health_player(jk)
-            break if alive == false
-            new.print_input
-            new.player_input
-            a = new.input[-1]
-            if checker(a) != true
-                new.health_damage
-            end
-                  
-        end      
+  def initialize(input = '', health = 5)
+    @chosen_word = ''
+    @underscored_word = ''
+    @player_word = []
+    puts 'Hangman started!'
+    puts 'What is your name?'
+    @name = gets.chomp
+    @alive = true
+    @input = input
+    @health = health
+    start
+  end
+
+  def start
+    new = Player.new(@name.to_s)
+    update_word
+    puts_health
+    update_underscored
+    turn
+  end
+
+  def turn
+    while @alive == true
+      check_health_player(@health)
+      break if alive == false
+      player_input
+      a = last_input
+      health_damage if checker(a) != true
+      serialize
     end
+  end
 
-    def checker(arg)
-        if @@chosen_word.include?(arg)
-            i = 0
-            until i == @@chosen_word.length
-                if @@chosen_word[i] == arg
-                    @@underscored_word[i] = arg                    
-                end
-                i += 1
-            end
-            p @@underscored_word
-            return true
-        end
-        @@player_word << arg
-        p "incorrect letters already used: #{@@player_word}"
-        false
+  def health_damage
+    puts health
+    self.health = @health - 1
+    puts_health
+  end
+
+  def puts_health
+    puts "you have #{health} tries remaining"
+  end
+
+  def player_input
+    puts 'Select a letter'
+    input = gets.chomp
+    until input.length == 1
+      puts 'invalid choice, try again'
+      input = gets.chomp
     end
-
-
+    change_input(input)
     
+  end
 
-    def update_word
-        j = Dictionary.new
-        @@chosen_word = j.word
+  def change_input(arg)
+    input_a = @input << arg
+    input = input_a
+  end
+
+  def print_input
+    puts @input
+  end
+
+  def last_input
+    @input[-1]
+  end
+
+  def checker(arg)
+    if @chosen_word.include?(arg)
+      i = 0
+      until i == @chosen_word.length
+        @underscored_word[i] = arg if @chosen_word[i] == arg
+        i += 1
+      end
+      p @underscored_word
+      return true
     end
+    @player_word << arg
+    puts @underscored_word
+    puts "incorrect letters already used: #{@player_word}"
+    false
+  end
 
-    def update_underscored
-        until @@underscored_word.length  == @@chosen_word.length
-                @@underscored_word << '_'        
-        end
-            puts @@underscored_word
-       
+  def update_word
+    j = Dictionary.new
+    @chosen_word = j.word
+  end
+
+  def update_underscored
+    @underscored_word << '_' until @underscored_word.length == @chosen_word.length
+    puts @underscored_word
+  end
+
+  def check_word
+    @alive = false if @chosen_word.eql? @underscored_word
+  end
+
+  def check_health_player(arg)
+    if arg.zero? == true
+      puts 'you lost'
+      self.alive = false
+    elsif @underscored_word.eql? @chosen_word
+      puts 'you won'
+      self.alive = false
     end
+  end
 
-    
-    
-
-    def check_word
-       if @@chosen_word.eql? @@underscored_word
-        @alive = false
-       end
-
-                
-    end
-
-
-    def check_health_player(arg)
-        if arg.zero? == true
-            puts "you lost"
-            self.alive = false
-        elsif @@underscored_word.eql? @@chosen_word
-            puts "you won"
-            self.alive = false
-        end
-    end
-
-    def alive
-        @alive
-    end
-
-    def winner
-        @winner
-    end
-    
-
-
-
-
-    
+  def serialize   
+    yaml = YAML.dump(self)
+  end
 end
 
 pedro = Game.new
-
-
-
